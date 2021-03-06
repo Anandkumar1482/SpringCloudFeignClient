e6f370b28534117b514acc0d3fcdffcc11c7c5aa
AnandKumar1482 <anand.dandi9@gmail.com>
2021-03-06 20:14:29 +0530

    Placeholders, read,conect-timeout added with Spring cloud OpenFeign

--- SpringCloudFeignClient/src/main/java/com/spring/service/FeignUserService.java
+++ SpringCloudFeignClient/src/main/java/com/spring/service/FeignUserService.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import com.spring.model.User;
 
-@FeignClient(name = "UserService", url = "http://localhost:8085")
+@FeignClient(name = "${service-name}", url = "${service.base.url}")
 public interface FeignUserService {
 
 	@GetMapping(value = "/user/{email}")
